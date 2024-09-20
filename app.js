fe7047cc69c93c549fad394f925bd066933ebc53
ramcujjw <ramcy.r77@gmail.com>
2024-09-20 10:51:35 +0530

    second commit

--- app.js
+++ app.js
@@ -5,25 +5,23 @@ app.use(morgan('dev'));
 
 
 //view
-app.set('view engine','ejs');
-app.set('views',__dirname+'/views');//define the path views is the keyword
+app.set('view engine','ejs');//ejs is a third party module ,html like markup kond varan vendi ann use cheyunnath
+app.set('views',__dirname+'/views');//define the path "views" is the keyword cannot be changed "/views" ia path
 
-//static files are  searched by node inside the public folder (7)
+//static files(changes ellate img ,doc ,pdf, css,js file) are  searched by node inside the public folder (7)
 app.use(express.static('public'));
 //(8) create a nav array for home and add employee link
 
-const nav=[
-    {
-        link :'/employee', name:'Home'
-    },{
-         link :'/employeeform', name:'Add Employee'
-    }
-]
+    const nav = [
+        { link: '/basic', name: 'Home' }, //field link(specify the path to that page) and name 
+        { link: '/basic/employeeform', name: 'Add Employee' } 
+    ];
+    
 
 
 //connecting routesfile
-const basicRoutes=require('./routes/basicRoutes')(nav);
-app.use('/basic',basicRoutes);
+const basicRoutes=require('./routes/basicRoutes')(nav);// requiring the router file it has to be exported and required.(!function calling here)
+app.use('/basic',basicRoutes);//we are specifying the path here
 
 
 app.listen(4000,()=>{
--- routes/basicRoutes.js
+++ routes/basicRoutes.js
@@ -2,18 +2,27 @@ const express=require('express');
 const router=new express.Router();
 //created a array
 
-var arr_obj=[{employeeid:19876,name:'Ramcy',post:'Developer',location:'Tvm'},{employeeid:12345,name:'Haripriya',post:'Tester',location:'Kollam'}]
 
 
 //write as a function(8)employeeform.ejs
-function basicRoutes(nav) {
-    
+function basicRoutes(nav) { //(this concept is only ised in ejs file)nav oru common function ayi use cheyunnu , so we are calling that as a parameter(!.1 this function is called at app.js)
+   const arr_obj=[{employeeid:19876,name:'Ramcy',post:'Developer',location:'Tvm'},{employeeid:12345,name:'Haripriya',post:'Tester',location:'Kollam'}]
+
+    router.get('/',(req,res)=>{ //here we specify the path ('/' /basic ) 
+        res.render('employee',{ //pass the data
+            title:'Employeelist',
+            arr_obj,
+            nav
+          
+        });
+    })
+ 
 
 router.get('/employeeform',(req,res)=>{
     // res.send(arr_obj);
     // giving render(5)
     res.render('employeeform',{ //pass the data
-        title:'Employeelist',
+        title:'add Employee',
         arr_obj,
         nav
     });
